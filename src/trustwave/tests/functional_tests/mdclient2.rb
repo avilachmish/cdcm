b4d598e6943e3fecc32074bea5c0df270a475bc8
rotem frenkel <rfrenkel@trustwave.com>
2019-09-09 11:14:26 +0300

    added test runner based on Ruby lang. can be used as client for the cdcm

---
+++ src/trustwave/tests/functional_tests/mdclient2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Majordomo Protocol client example. Uses the mdcliapi2 API to hide all MDP aspects
+#
+# Author : Tom van Leeuwen <tom@vleeuwen.eu>
+# Based on Python example by Min RK
+
+require './mdcliapi2.rb'
+require './testRunner.rb'
+require './myLogger.rb'
+require 'json'
+
+runner = Test_Runner.new
+runner.run_test_from_file("actions.xml")
+END {
+    puts "closing log file"
+   log.close
+}
---
+++ src/trustwave/tests/functional_tests/myLogger.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# https://ruby-doc.org/stdlib-2.6.3/libdoc/logger/rdoc/Logger.html
+# TODO: write destructor that close the log file: @logger.close https://stackoverflow.com/questions/5956067/ruby-destructors
+class MyLogger
+    include Singleton
+    def initialize
+        #log_file = File.open('cdcm_client.log', File::WRONLY |  File::CREAT | File::APPEND )
+        log_file = File.open('cdcm_client.log', File::WRONLY | File::CREAT | File::TRUNC)
+
+        @logger = Logger.new( log_file )
+        @logger.datetime_format = '%d--%m-%Y %H:%M:%S '
+        @logger.formatter = proc do |severity, datetime, progname, msg|
+            "[#{severity}] [#{datetime}] [#{progname}:#{__LINE__}]: #{msg}\n"
+
+        end
+    end
+
+    def log
+        @logger
+    end
+
+end #end of class MyLogger
