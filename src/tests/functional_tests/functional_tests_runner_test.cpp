2180dd8670d61a586df7c9299381ac562714872a
Assaf Cohen <ascohen@trustwave.com>
2019-09-30 20:23:02 +0300

    log in appropriate location

--- src/tests/functional_tests/functional_tests_runner_test.cpp
+++ src/tests/functional_tests/functional_tests_runner_test.cpp
@@ -25,7 +25,7 @@ BOOST_AUTO_TEST_SUITE(Functional)
 
     BOOST_AUTO_TEST_CASE(standard) {
 
-         boost::asio::io_service ios;
+        boost::asio::io_service ios;
         trustwave::wrapper w(ios);
         std::future<std::error_code> e;
         auto pa = std::make_pair(std::future<std::string>(),std::future<std::string>());
