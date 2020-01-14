14e79116d79ac89b475e581f59ed433907b10e2a
Assaf Cohen <ascohen@trustwave.com>
2020-01-14 14:58:27 +0200

    formatting rules added and applied

--- src/tests/functional_tests/functional_tests_runner_test.cpp
+++ src/tests/functional_tests/functional_tests_runner_test.cpp
@@ -3,7 +3,7 @@
 //														functional_tests_runner_test.cpp
 //
 //---------------------------------------------------------------------------------------------------------------------
-// DESCRIPTION: 
+// DESCRIPTION:
 //
 //
 //---------------------------------------------------------------------------------------------------------------------
@@ -14,53 +14,52 @@
 //                          						Include files
 //=====================================================================================================================
 #define BOOST_TEST_DYN_LINK
+#include "../components/boost_test_wrapper/wrapper.hpp"
+#include <boost/algorithm/string.hpp>
 #include <boost/container/flat_map.hpp>
 #include <boost/range/iterator_range.hpp>
 #include <boost/range/iterator_range_io.hpp>
-#include <boost/test/unit_test.hpp>
-#include <boost/algorithm/string.hpp>
 #include <boost/regex.hpp>
-#include "../components/boost_test_wrapper/wrapper.hpp"
+#include <boost/test/unit_test.hpp>
 #include <string>
 BOOST_AUTO_TEST_SUITE(Functional)
 
-    BOOST_AUTO_TEST_CASE(standard) {
+BOOST_AUTO_TEST_CASE(standard)
+{
+    boost::asio::io_service ios;
+    trustwave::wrapper w(ios);
+    std::future<std::error_code> e;
+    auto pa = std::make_pair(std::future<std::string>(), std::future<std::string>());
+    auto p = w.start_external_test("exe_test", "./functional_tests/actions.xml", std::move(pa), std::move(e));
+    ios.run();
+    auto out = pa.first.get();
+    auto err = pa.second.get();
+    std::vector<std::string> cont;
+    boost::split(cont, out, boost::is_any_of("\n"));
+    BOOST_TEST_MESSAGE("Ran " << cont.size() << " tests");
+    boost::regex re(".*:(.*) .*:(.*) .*:(.*) .*:(.*) .*:(.*) .*:\\[(.*)\\]");
+    const int subs[] = {1};
 
-        boost::asio::io_service ios;
-        trustwave::wrapper w(ios);
-        std::future<std::error_code> e;
-        auto pa = std::make_pair(std::future<std::string>(),std::future<std::string>());
-        auto p = w.start_external_test("exe_test","./functional_tests/actions.xml",std::move(pa),std::move(e));
-        ios.run();
-        auto out = pa.first.get();
-        auto err = pa.second.get();
-        std::vector<std::string > cont;
-        boost::split(cont, out, boost::is_any_of("\n"));
-        BOOST_TEST_MESSAGE("Ran "<<cont.size()<<" tests");
-        boost::regex re(".*:(.*) .*:(.*) .*:(.*) .*:(.*) .*:(.*) .*:\\[(.*)\\]");
-        const int subs[] = {1};
+    for(auto line: cont) {
+        if(boost::starts_with(line, "result")) {
+            enum {
+                result = 1,
+                ip,
+                action_name,
+                session_name,
+                req_id,
+                reason
 
-        for (auto line:cont)
-        {
-            if(boost::starts_with(line, "result")) {
-
-                 enum {
-                    result=1,
-                    ip,
-                    action_name,
-                    session_name,
-                    req_id,
-                    reason
-
-                };
-                boost::regex_token_iterator<std::string::iterator> it(line.begin(), line.end(),re,subs );
-                boost::regex_token_iterator<std::string::iterator> end;
-                boost::smatch what;
-                if (boost::regex_search(line, what, re))
-                {
-                    BOOST_TEST(what[result] == "passed","Test: '"<<what[action_name]<<"' of session: '"<<what[session_name]<< "' with reason: '"<<what[reason]<<"'");
-                }
+            };
+            boost::regex_token_iterator<std::string::iterator> it(line.begin(), line.end(), re, subs);
+            boost::regex_token_iterator<std::string::iterator> end;
+            boost::smatch what;
+            if(boost::regex_search(line, what, re)) {
+                BOOST_TEST(what[result] == "passed", "Test: '" << what[action_name] << "' of session: '"
+                                                               << what[session_name] << "' with reason: '"
+                                                               << what[reason] << "'");
             }
         }
+    }
 }
 BOOST_AUTO_TEST_SUITE_END()
