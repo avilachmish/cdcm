a14eedf14a35b092c334ecbfdf7b4b9b8e62da90
Assaf Cohen <ascohen@trustwave.com>
2020-01-22 12:54:27 +0200

    updated rules for build/test
    Former-commit-id: 81daa3f7af327fa95c917313b540c9c0f3362224

--- src/backend/services/winrm/actions/enumerate.cpp
+++ src/backend/services/winrm/actions/enumerate.cpp
@@ -19,7 +19,14 @@
 #include "singleton_runner/authenticated_scan_server.hpp"
 #include "client/winrm_client.hpp"
 using trustwave::Winrm_Enumerate_Action;
-
+auto push_back = [](tao::json::events::to_value& c, const std::string& k, const std::string& v) {
+    c.begin_object();
+    c.key(k);
+    c.string(v);
+    c.member();
+    c.end_object();
+    c.element();
+};
 int Winrm_Enumerate_Action::act(boost::shared_ptr<session> sess, std::shared_ptr<action_msg> action,
                                 std::shared_ptr<result_msg> res)
 {
@@ -29,16 +36,13 @@ int Winrm_Enumerate_Action::act(boost::shared_ptr<session> sess, std::shared_ptr
     }
     winrm_client cli(sess->remote(), 5985, "/wsman", "http", "Basic", sess->creds().username(),
                         sess->creds().password());
-    const std::string r{"http://schemas.microsoft.com/wbem/wsman/1/wmi/root/cimv2/Win32_Service"};
-    std::vector<std::string> res2;
-    trustwave::winrm_filter filt("http://schemas.microsoft.com/wbem/wsman/1/WQL",
-                                "SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IpEnabled=TRUE");
-
     auto winrm_enumerate_action = std::dynamic_pointer_cast<winrm_action_enumerate_msg>(action);
+    trustwave::winrm_filter filt("http://schemas.microsoft.com/wbem/wsman/1/WQL",
+                                 winrm_enumerate_action->filter_);
     std::vector<std::string> enumerate_res;
-    res->res(cli.Get(winrm_enumerate_action->uri_, std::addressof(winrm_get_action->selectors_) ));
-    cli.Enumerate(r, filt, res2);
-
+    cli.Enumerate(winrm_enumerate_action->uri_,filt ,enumerate_res);
+    const tao::json::value v = enumerate_res;
+    res->res(to_string(v, 2));
     return 0;
 }
 
