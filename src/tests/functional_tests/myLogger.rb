193727778731c4bd37a7e9756001b0870c3a7f6f
rotem frenkel <rfrenkel@trustwave.com>
2020-03-24 13:59:35 +0200

    added testsRunner_settings.json file for configuring the clients log dir, edited actions1.xml
    Former-commit-id: 051c99c2771e543231fd38bf3dad892bf0790b97

--- src/tests/functional_tests/myLogger.rb
+++ src/tests/functional_tests/myLogger.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 require 'fileutils'
+require 'json'
 # https://ruby-doc.org/stdlib-2.6.3/libdoc/logger/rdoc/Logger.html
 # TODO: write destructor that close the log file: @logger.close https://stackoverflow.com/questions/5956067/ruby-destructors
 class MyLogger
     include Singleton
     def initialize
-        response = FileUtils.mkdir_p('/var/log/cdcm/testsRunner')
-
-        #log_file = File.open('cdcm_client.log', File::WRONLY |  File::CREAT | File::APPEND )
-        log_file = File.open("/var/log/cdcm/testsRunner/cdcm_client#{$client_id}.log", File::WRONLY | File::CREAT | File::TRUNC)
+        
+        settings_file = File.open "./testsRunner_settings.json"
+        settings_data = JSON.load settings_file
+        settings_file.close
+        logs_dir = settings_data["logger"]["logs_dir"].to_s
+        response = FileUtils.mkdir_p(logs_dir)
+        log_file = File.open("#{logs_dir}/cdcm_client#{$client_id}.log", File::WRONLY | File::CREAT | File::TRUNC)
         log_file.sync = true
         @logger = Logger.new( log_file )
         @logger.datetime_format = '%d-%m-%Y %H:%M:%S::%3N'
